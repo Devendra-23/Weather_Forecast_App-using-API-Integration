442744e2fc46f5ddb7cf1e1a4541a392246573ba
Devendra-23 <dev6dhokare@gmail.com>
2024-01-24 03:54:48 +0000

    Removed name suggestion bug

--- weatherproject/weatherproject/settings.py
+++ weatherproject/weatherproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-3ch-crl^+#)hvya=x5=hdyo#i$zbk7c)_@gd15m1+*6ei8yed(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
